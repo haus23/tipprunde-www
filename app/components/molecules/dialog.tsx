d316dce2455cdff2e7caccca8a4b459d9d5044e8
Micha Buchholz <micha@haus23.net>
2023-05-30 05:34:52 +0200

    feat: Add reusable dialog.

---
+++ app/components/molecules/dialog.tsx
@@ -0,0 +1,29 @@
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { forwardRef } from 'react';
+import { cn } from '~/utils';
+
+const Dialog = DialogPrimitive.Root;
+const DialogTrigger = DialogPrimitive.Trigger;
+const DialogTitle = DialogPrimitive.Title;
+
+const DialogContent = forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <DialogPrimitive.Portal>
+    <DialogPrimitive.Overlay className="fixed inset-0 bg-background/20 backdrop-blur-sm transition-all duration-100 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in" />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        'fixed inset-4 mx-auto max-w-xl rounded-md bg-subtle shadow-md ring-1 ring-line animate-in focus:outline-none data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 sm:zoom-in-90 data-[state=open]:sm:slide-in-from-bottom-0',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </DialogPrimitive.Content>
+  </DialogPrimitive.Portal>
+));
+DialogContent.displayName = DialogPrimitive.Content.displayName;
+
+export { Dialog, DialogTrigger, DialogTitle, DialogContent };
