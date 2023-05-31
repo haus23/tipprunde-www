7bf687e891d45d26e795916426efca97b927ea67
Micha Buchholz <micha@haus23.net>
2023-05-31 12:14:54 +0200

    refactor: Optimize data loading.

--- app/routes/_app.($championship)/route.tsx
+++ app/routes/_app.($championship)/route.tsx
@@ -1,9 +1,13 @@
 import { json, type LoaderArgs } from '@remix-run/node';
-import { Outlet } from '@remix-run/react';
+import { Outlet, type ShouldRevalidateFunction } from '@remix-run/react';
 import { fetchChampionshipPlayers } from '~/backend/queries';
 
 export const loader = async ({ params }: LoaderArgs) => {
   return json(await fetchChampionshipPlayers(params.championship));
+};
+
+export const shouldRevalidate: ShouldRevalidateFunction = ({ currentParams, nextParams }) => {
+  return currentParams.championship !== nextParams.championship;
 };
 
 export default function StandingsLayout() {
--- app/routes/_app/route.tsx
+++ app/routes/_app/route.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from '@remix-run/react';
+import { Outlet, type ShouldRevalidateFunction } from '@remix-run/react';
 import { AppHeader } from './app-header';
 import { json } from '@remix-run/node';
 import { fetchChampionships } from '~/backend/queries';
 
 export const loader = async () => {
   return json(await fetchChampionships());
+};
+
+export const shouldRevalidate: ShouldRevalidateFunction = () => {
+  return false;
 };
 
 export default function AppLayout() {
