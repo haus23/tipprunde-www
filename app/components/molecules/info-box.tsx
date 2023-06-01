163b521349ed3a40940a5670f440bcd5cf6bda66
Micha Buchholz <micha@haus23.net>
2023-06-01 15:35:04 +0200

    feat: Make icon customizable.

--- app/components/molecules/info-box.tsx
+++ app/components/molecules/info-box.tsx
@@ -4,17 +4,21 @@ import {
   type ElementRef,
   useState,
   useRef,
+  type ElementType,
 } from 'react';
 import * as PopoverPrimitive from '@radix-ui/react-popover';
 import { useHover } from '@react-aria/interactions';
 import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import { cn } from '~/utils';
 
-type InfoBoxProps = ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>;
+type InfoBoxProps = ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> & {
+  icon?: ElementType;
+};
+
 type InfoBoxRef = ElementRef<typeof PopoverPrimitive.Content>;
 
 const InfoBox = forwardRef<InfoBoxRef, InfoBoxProps>(
-  ({ className, children, sideOffset = 2, ...props }, ref) => {
+  ({ className, children, icon, sideOffset = 2, ...props }, ref) => {
     const [isOpen, setOpen] = useState(false);
     const debounceRef = useRef(false);
 
@@ -32,19 +36,21 @@ const InfoBox = forwardRef<InfoBoxRef, InfoBoxProps>(
       },
     });
 
+    const TriggerIcon = icon ?? InformationCircleIcon;
+
     return (
       <PopoverPrimitive.Root open={isOpen} onOpenChange={setOpen}>
         <PopoverPrimitive.Trigger
           {...hoverProps}
           className="rounded focus:outline-none focus-visible:outline focus-visible:outline-ring"
         >
-          <InformationCircleIcon className="h-5" />
+          <TriggerIcon className="h-5" />
         </PopoverPrimitive.Trigger>
         <PopoverPrimitive.Content
           ref={ref}
           sideOffset={sideOffset}
           className={cn(
-            'rounded-md bg-subtle text-accent-foreground shadow-md outline-none ring-2 ring-ring animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+            'rounded-md bg-subtle text-accent-foreground shadow-md outline-none ring-1 ring-ring animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
             className
           )}
           {...props}
