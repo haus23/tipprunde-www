eac51d05d97ffaab26e2c037a3d714cd902fbaf5
Micha Buchholz <micha@haus23.net>
2023-06-06 12:02:44 +0200

    refactor: Make helpers error prune.

--- app/utils/route-match-helper.ts
+++ app/utils/route-match-helper.ts
@@ -1,9 +1,14 @@
-import type { Championship, Matches, Player } from '@haus23/tipprunde-types';
+import type { SerializeFrom } from '@remix-run/node';
 import type { RouteMatch } from '@remix-run/react';
 
+import type { loader as rootLoader } from '~/root';
+import type { loader as championshipLayoutLoader } from '~/routes/($championship)+/_layout';
+import type { loader as tippDetailsLoader } from '~/routes/($championship)+/tipps+/_layout';
+
 export function getChampionships(matches: RouteMatch[]) {
-  const appMatch = matches.find((m) => m.id === 'root');
-  return appMatch?.data.championships as Championship[];
+  const match = matches.find((m) => m.id === 'root');
+  const data = match?.data as SerializeFrom<typeof rootLoader>;
+  return data.championships;
 }
 
 export function getChampionship(championshipId: string | undefined, matches: RouteMatch[]) {
@@ -14,11 +19,15 @@ export function getChampionship(championshipId: string | undefined, matches: Rou
 }
 
 export function getChampionshipPlayers(matches: RouteMatch[]) {
-  const championshipMatch = matches.find((m) => m.id === 'routes/($championship)+/_layout');
-  return championshipMatch?.data.players as Player[];
+  const match = matches.find((m) => m.id === 'routes/($championship)+/_layout');
+
+  const data = match?.data as SerializeFrom<typeof championshipLayoutLoader>;
+  return data.players;
 }
 
 export function getChampionshipMatches(matches: RouteMatch[]) {
-  const tippsMatch = matches.find((m) => m.id === 'routes/($championship)+/tipps+/_layout');
-  return tippsMatch?.data.matches as Matches;
+  const match = matches.find((m) => m.id === 'routes/($championship)+/tipps+/_layout');
+
+  const data = match?.data as SerializeFrom<typeof tippDetailsLoader>;
+  return data.matches;
 }
