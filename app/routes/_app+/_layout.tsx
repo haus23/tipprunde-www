7517ca32233e4c8e3bac5fbafdc002e143af254a
Micha Buchholz <micha@haus23.net>
2023-06-06 23:09:54 +0200

    feat: Validate championship.

--- app/routes/_app+/_layout.tsx
+++ app/routes/_app+/_layout.tsx
@@ -1,9 +1,19 @@
-import { json } from '@remix-run/node';
+import { json, type LoaderArgs } from '@remix-run/node';
 import { Outlet, type ShouldRevalidateFunction } from '@remix-run/react';
 import { fetchChampionships } from '~/backend/queries';
 
-export const loader = async () => {
+export const loader = async ({ params }: LoaderArgs) => {
+  const { championship: championshipId } = params;
+
   const championships = await fetchChampionships();
+
+  if (championshipId && !championships.find((c) => c.id === championshipId)) {
+    throw new Response(null, {
+      status: 404,
+      statusText: 'Not Found',
+    });
+  }
+
   return json({ championships });
 };
 
