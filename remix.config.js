cc9ad63c4be2581e53a6ec7c2e8dcd32efbbd118
Micha Buchholz <micha@haus23.net>
2023-11-16 19:14:27 +0100

    refactor: remove obsolete flags.

--- remix.config.js
+++ remix.config.js
@@ -7,20 +7,6 @@ module.exports = {
   routes: async (defineRoutes) => {
     return flatRoutes('routes', defineRoutes);
   },
-
-  // Features
-  tailwind: true,
-
   // V2 Default Opt-Out
   serverModuleFormat: 'cjs',
-
-  // V2 Preview
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 };
