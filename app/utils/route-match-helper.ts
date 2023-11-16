8b58a7a1dd941a8bff6bbe8ddd7aab0ab726b2c1
Micha Buchholz <micha@haus23.net>
2023-11-16 19:02:24 +0100

    refactor: Adapt matches type to the new meta function argument.

--- app/utils/route-match-helper.ts
+++ app/utils/route-match-helper.ts
@@ -1,31 +1,33 @@
 import type { SerializeFrom } from '@remix-run/node';
-import type { RouteMatch } from '@remix-run/react';
+import type { MetaArgs, UIMatch } from '@remix-run/react';
 
 import type { loader as appLoader } from '~/routes/_app+/_layout';
 import type { loader as championshipLayoutLoader } from '~/routes/_app+/($championship)+/_layout';
 import type { loader as tippDetailsLoader } from '~/routes/_app+/($championship)+/tipps+/_layout';
 
-export function getChampionships(matches: RouteMatch[]) {
+type RouteMatches = MetaArgs['matches'] | UIMatch[];
+
+export function getChampionships(matches: RouteMatches) {
   const match = matches.find((m) => m.id === 'routes/_app+/_layout');
   const data = match?.data as SerializeFrom<typeof appLoader>;
   return data.championships;
 }
 
-export function getChampionship(championshipId: string | undefined, matches: RouteMatch[]) {
+export function getChampionship(championshipId: string | undefined, matches: RouteMatches) {
   const championships = getChampionships(matches);
 
   const championship = championships.find((c) => c.id === championshipId) || championships[0];
   return championship;
 }
 
-export function getChampionshipPlayers(matches: RouteMatch[]) {
+export function getChampionshipPlayers(matches: RouteMatches) {
   const match = matches.find((m) => m.id === 'routes/_app+/($championship)+/_layout');
 
   const data = match?.data as SerializeFrom<typeof championshipLayoutLoader>;
   return data.players;
 }
 
-export function getChampionshipMatches(matches: RouteMatch[]) {
+export function getChampionshipMatches(matches: RouteMatches) {
   const match = matches.find((m) => m.id === 'routes/_app+/($championship)+/tipps+/_layout');
 
   const data = match?.data as SerializeFrom<typeof tippDetailsLoader>;
