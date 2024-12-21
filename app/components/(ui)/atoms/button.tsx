2a05dcd3dad7676e6b854613915974aa48a127f6
Micha Buchholz <micha@haus23.net>
2024-12-21 19:57:30 +0100

    feat: Show off simplified modernized component.

--- app/components/(ui)/atoms/button.tsx
+++ app/components/(ui)/atoms/button.tsx
@@ -1,36 +1,29 @@
-import { forwardRef, type ButtonHTMLAttributes } from 'react';
-import { cva, type VariantProps } from 'cva';
-import { cn } from '~/utils';
+import { type VariantProps, tv } from 'tailwind-variants';
 
-const buttonVariants = cva(
-  'inline-flex items-center justify-center font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
-  {
-    variants: {
-      variant: {
-        default: 'rounded-md',
-        toolbar: 'bg-primary rounded-lg hover:bg-primary-hover',
-      },
-      size: {
-        default: 'px-4 py-2',
-      },
+const styles = tv({
+  base: 'inline-flex items-center justify-center font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  variants: {
+    variant: {
+      default: 'rounded-md',
+      toolbar: 'bg-primary rounded-lg hover:bg-primary-hover',
     },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
+    size: {
+      default: 'px-4 py-2',
     },
-    compoundVariants: [{ variant: 'toolbar', size: 'default', class: 'px-2' }],
-  }
-);
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+  compoundVariants: [{ variant: 'toolbar', size: 'default', class: 'px-2' }],
+});
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof buttonVariants>;
+namespace Button {
+  export interface Props
+    extends React.ComponentProps<'button'>,
+      VariantProps<typeof styles> {}
+}
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
-    return (
-      <button className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
-    );
-  }
-);
-Button.displayName = 'Button';
-
-export { Button };
+export function Button({ className, size, variant, ...props }: Button.Props) {
+  return <button className={styles({ className, size, variant })} {...props} />;
+}
