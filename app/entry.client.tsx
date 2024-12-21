6e1537b5324d59989f42b8df7c46844232396057
Micha Buchholz <micha@haus23.net>
2024-12-21 13:41:41 +0100

    feat: Add tailwind and theme.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import './styles/tailwind.css';
+
 const container = document.getElementById('root');
 if (!container) throw Error('Missing root element!');
 
 createRoot(container).render(
   <StrictMode>
-    <span>runde.tips</span>
+    <span className="text-2xl">runde.tips</span>
   </StrictMode>,
 );
