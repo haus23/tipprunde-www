feef2f6a37b28e3389009c6b3c5ffa1e6a2627aa
Micha Buchholz <micha@haus23.net>
2024-12-22 22:19:50 +0100

    feat: Implement the current tips infoboxes.

--- app/components/(ui)/molecules/info-box.tsx
+++ app/components/(ui)/molecules/info-box.tsx
@@ -1,70 +1,65 @@
-import {
-  forwardRef,
-  type ComponentPropsWithoutRef,
-  type ElementRef,
-  useState,
-  useRef,
-  type ElementType,
-} from 'react';
-import * as PopoverPrimitive from '@radix-ui/react-popover';
-import { useHover } from '@react-aria/interactions';
 import { InformationCircleIcon } from '@heroicons/react/24/outline';
-import { cn } from '~/utils';
+import * as PopoverPrimitive from '@radix-ui/react-popover';
+import { type ElementType, useRef, useState } from 'react';
+import { useHover } from 'react-aria';
+import { tv } from 'tailwind-variants';
 
-type InfoBoxProps = ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> & {
-  icon?: ElementType;
-  ariaTriggerLabel?: string;
-};
+const styles = tv({
+  base: 'rounded-md bg-subtle text-accent-foreground shadow-md outline-none ring-1 ring-ring animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+});
 
-type InfoBoxRef = ElementRef<typeof PopoverPrimitive.Content>;
-
-const InfoBox = forwardRef<InfoBoxRef, InfoBoxProps>(
-  ({ className, children, icon, sideOffset = 2, ariaTriggerLabel, ...props }, ref) => {
-    const [isOpen, setOpen] = useState(false);
-    const debounceRef = useRef(false);
-
-    const { hoverProps } = useHover({
-      onHoverChange(isHovering) {
-        if (isHovering) {
-          debounceRef.current = true;
-          setOpen(true);
-          setTimeout(() => {
-            debounceRef.current = false;
-          }, 50);
-        } else {
-          if (!debounceRef.current) setOpen(false);
-        }
-      },
-    });
-
-    const TriggerIcon = icon ?? InformationCircleIcon;
-
-    return (
-      <PopoverPrimitive.Root open={isOpen} onOpenChange={setOpen}>
-        <PopoverPrimitive.Trigger
-          aria-label={ariaTriggerLabel}
-          {...hoverProps}
-          className="rounded focus:outline-none focus-visible:outline focus-visible:outline-ring"
-        >
-          <TriggerIcon className="h-5" />
-        </PopoverPrimitive.Trigger>
-        <PopoverPrimitive.Content
-          ref={ref}
-          sideOffset={sideOffset}
-          className={cn(
-            'rounded-md bg-subtle text-accent-foreground shadow-md outline-none ring-1 ring-ring animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-            className
-          )}
-          {...props}
-          {...hoverProps}
-        >
-          <PopoverPrimitive.Arrow className="h-2 w-4 fill-ring" />
-          {children}
-        </PopoverPrimitive.Content>
-      </PopoverPrimitive.Root>
-    );
+namespace InfoBox {
+  export interface Props extends PopoverPrimitive.PopoverContentProps {
+    icon?: ElementType;
+    ariaTriggerLabel?: string;
   }
-);
-InfoBox.displayName = 'InfoBox';
+}
 
-export { InfoBox };
+export function InfoBox({
+  className,
+  children,
+  icon,
+  sideOffset = 2,
+  ariaTriggerLabel,
+  ...props
+}: InfoBox.Props) {
+  const [isOpen, setOpen] = useState(false);
+  const debounceRef = useRef(false);
+
+  const { hoverProps } = useHover({
+    onHoverChange(isHovering) {
+      if (isHovering) {
+        debounceRef.current = true;
+        setOpen(true);
+        setTimeout(() => {
+          debounceRef.current = false;
+        }, 50);
+      } else {
+        if (!debounceRef.current) setOpen(false);
+      }
+    },
+  });
+
+  const TriggerIcon = icon ?? InformationCircleIcon;
+
+  return (
+    <PopoverPrimitive.Root open={isOpen} onOpenChange={setOpen}>
+      <PopoverPrimitive.Trigger
+        aria-label={ariaTriggerLabel}
+        {...hoverProps}
+        className="rounded focus:outline-none focus-visible:outline focus-visible:outline-ring"
+      >
+        <TriggerIcon className="h-5" />
+      </PopoverPrimitive.Trigger>
+      <PopoverPrimitive.Content
+        sideOffset={sideOffset}
+        className={styles({ className })}
+        {...props}
+        {...hoverProps}
+      >
+        <PopoverPrimitive.Arrow className="h-2 w-4 fill-ring" />
+        {children}
+      </PopoverPrimitive.Content>
+    </PopoverPrimitive.Root>
+  );
+}
--- app/routes/tables/_route.tsx
+++ app/routes/tables/_route.tsx
@@ -1,5 +1,8 @@
+import { CalendarDaysIcon } from '@heroicons/react/24/outline';
 import { useLoaderData } from 'react-router';
+import { Fragment } from 'react/jsx-runtime';
 import { Link } from '#/components/(ui)/atoms/link';
+import { InfoBox } from '#/components/(ui)/molecules/info-box';
 import { useChampionship } from '#/utils/app/championship';
 import { usePlayers } from '#/utils/app/players';
 import type { tablesLoader } from './_route.data';
@@ -9,7 +12,7 @@ export default function TablesRoute() {
   const players = usePlayers(championship.id);
 
   const { currentTips } = useLoaderData<ReturnType<typeof tablesLoader>>();
-  console.log(currentTips);
+
   return (
     <div>
       <header className="mx-2 flex items-center gap-x-2 pt-2 text-accent-foreground sm:mx-0 sm:gap-x-4">
@@ -83,6 +86,70 @@ export default function TablesRoute() {
                 <td className="px-2 text-center last:pr-4 md:px-6">
                   {p.totalPoints}
                 </td>
+                {!championship.completed && (
+                  <td>
+                    <div className="flex items-center pr-2">
+                      <InfoBox
+                        side="top"
+                        collisionPadding={8}
+                        icon={CalendarDaysIcon}
+                        ariaTriggerLabel={`Aktuelle Tips von ${p.account.name}`}
+                      >
+                        <div className="grid w-[240px] grid-cols-[1fr_repeat(2,_auto)] pb-2 text-sm font-normal">
+                          <div className="border-b border-line py-2 pl-2">
+                            Spiel
+                          </div>
+                          <div className="border-b border-line p-2 text-center">
+                            Tipp
+                          </div>
+                          <div className="border-b border-line p-2 text-center">
+                            Pkt
+                          </div>
+                          {currentTips.map((m) => {
+                            const tip = m.tips[p.id];
+                            return (
+                              <Fragment key={m.matchId}>
+                                <div
+                                  className={[
+                                    'py-1 pl-2',
+                                    (tip?.joker || tip?.lonelyHit) &&
+                                      'bg-primary',
+                                  ]
+                                    .filter(Boolean)
+                                    .join(' ')}
+                                >
+                                  {m.hometeam}-{m.awayteam}
+                                </div>
+                                <div
+                                  className={[
+                                    'py-1 text-center',
+                                    (tip?.joker || tip?.lonelyHit) &&
+                                      'bg-primary',
+                                  ]
+                                    .filter(Boolean)
+                                    .join(' ')}
+                                >
+                                  {tip?.tip}
+                                </div>
+                                <div
+                                  className={[
+                                    'py-1 text-center',
+                                    (tip?.joker || tip?.lonelyHit) &&
+                                      'bg-primary',
+                                  ]
+                                    .filter(Boolean)
+                                    .join(' ')}
+                                >
+                                  {m.result && tip?.points}
+                                </div>
+                              </Fragment>
+                            );
+                          })}
+                        </div>
+                      </InfoBox>
+                    </div>
+                  </td>
+                )}
               </tr>
             );
           })}
